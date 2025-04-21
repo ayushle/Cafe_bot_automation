bee74418aa17da125409450038d36193494865f6
ayushle <125122562+ayushle@users.noreply.github.com>
2025-04-22 02:08:41 +0530

    Delete build directory

--- build/cafe_bot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ayush/Documents/cafeBot_ws/src/cafe_bot
-# Build directory: /home/ayush/Documents/cafeBot_ws/build/cafe_bot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/cafe_bot/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(cafe_bot_IDL_FILES "msg/Order.idl")
-set(cafe_bot_INTERFACE_FILES "msgs/Order.msg")
